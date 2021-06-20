1c121934ca9d315b4fd70f9f9f9efb7aa1d931ce
ventu87 <ventu87@hotmail.it>
2021-06-21 00:30:24 +0200

    fix func toggle without mutation

--- prove.js
+++ prove.js
@@ -1,4 +1,4 @@
-function toggleTodo(arr,index){
+/*function toggleTodo(arr,index){
     const newArr=[...arr];
     
     if(index>arr.length || index < 0){
@@ -11,10 +11,31 @@ function toggleTodo(arr,index){
        newArr[index].completed=!newArr[index].completed;
         };
        return newArr;
-      }
-    
+      }*/
 
-    const arr=[{todo:'pane',completed:false}]
+function toggleTodo(arr, index) {
+	if (index > arr.length || index < 0) {
+		throw new Error("the index that you have insert is inexistent");
+	}
 
-    console.log(toggleTodo(arr,0));
-    console.log(toggleTodo(arr,0));
+	if (index === "" || index === undefined) {
+		return arr;
+	} else {
+		return arr.map((elem, index2, arr) => {
+			if (index !== index2) {
+				return elem;
+			} else {
+				return { todo: elem.todo, completed: !elem.completed };
+			}
+		});
+	}
+}
+
+const arr = [
+	{ todo: "pane", completed: false },
+	{ todo: "latte", completed: false },
+	{ todo: "banane", completed: false },
+];
+
+console.log(toggleTodo(arr, 0));
+//console.log(toggleTodo(arr, 0));
--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,106 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 /**
- * 
- * @param {Array<{text: string, completed: boolean}>} arr 
- * @param {string} string 
+ *
+ * @param {Array<{text: string, completed: boolean}>} arr
+ * @param {string} string
  * @returns {Array<{text: string, completed: boolean}>}s
  */
 function addItem(arr, string) {
-  if (string === "") {
-    return arr;
-  }
-  return arr.concat([
-    {
-      todo: string,
-      completed: false,
-    },
-  ]);
+	if (string === "") {
+		return arr;
+	}
+	return arr.concat([
+		{
+			todo: string,
+			completed: false,
+		},
+	]);
 }
 
 /**
- * 
- * @param {Array<{text: string, completed: boolean}>} arr 
- * @param {number} index 
+ *
+ * @param {Array<{text: string, completed: boolean}>} arr
+ * @param {number} index
  * @returns {Array<{text: string, completed: boolean}>
  */
-function deleteTodo(arr,index){
-  if(index>arr.length || index < 0){
-    throw new Error('the index that you have insert is inexistent')
-  }
+function deleteTodo(arr, index) {
+	if (index > arr.length || index < 0) {
+		throw new Error("the index that you have insert is inexistent");
+	}
 
-  if(index === "" || index === undefined){
-    return arr;
-  }else{
-    return arr.filter(elem=>{
-      return elem !== arr[index];
-    });
-  }
+	if (index === "" || index === undefined) {
+		return arr;
+	} else {
+		return arr.filter((elem) => {
+			return elem !== arr[index];
+		});
+	}
 }
 
-function toggleTodo(arr,index){
-  const newArr=[...arr];
-  
-  if(index>arr.length || index < 0){
-    throw new Error('the index that you have insert is inexistent')
-  }
+function toggleTodo(arr, index) {
+	if (index > arr.length || index < 0) {
+		throw new Error("the index that you have insert is inexistent");
+	}
 
-  if(index === "" || index === undefined){
-    return arr;
-  }else if(arr[index]){
-     newArr[index].completed=!newArr[index].completed;
-      };
-     return newArr;
-    }
-
-
-
-
-
+	if (index === "" || index === undefined) {
+		return arr;
+	} else {
+		return arr.map((elem) => {
+			if (elem !== arr[index]) {
+				return elem;
+			} else {
+				elem.completed = !elem.completed;
+				return elem;
+			}
+		});
+	}
+}
 
 function App() {
-  
-  const [todos, setTodos] = useState([])
-  const [inputText,setInput] = useState('')
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <h1>App Todo By Marco Venturini</h1>
-        <p>
-          <form onSubmit={e  => {
-            e.preventDefault()
-            setTodos(
-              addItem(todos,inputText)
-            )
-          }}>
-            <input id="inputTodo" onInput={(evt)=>setInput(evt.target.value)}type="text" />
-            <button type="submit">Submit</button>
-          </form>
-          
-        </p>
-        {todos.map((todo,index) =>(
-          <li className={todo.completed?'completed':''}>{todo.todo} <button type="button" onClick={()=>setTodos(deleteTodo(todos,index))} >Rimuovi</button> <button type="button" onClick={()=>setTodos(toggleTodo(todos,index))}>Completa</button></li>
-        ))}
-        
-      </header>
-     
-    </div>
-  )
+	const [todos, setTodos] = useState([]);
+	const [inputText, setInput] = useState("");
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>App Todo By Marco Venturini</h1>
+				<p>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							setTodos(addItem(todos, inputText));
+						}}
+					>
+						<input
+							id="inputTodo"
+							onInput={(evt) => setInput(evt.target.value)}
+							type="text"
+						/>
+						<button type="submit">Submit</button>
+					</form>
+				</p>
+				{todos.map((todo, index) => (
+					<li className={todo.completed ? "completed" : ""}>
+						{todo.todo}{" "}
+						<button
+							type="button"
+							onClick={() => setTodos(deleteTodo(todos, index))}
+						>
+							Rimuovi
+						</button>{" "}
+						<button
+							type="button"
+							onClick={() => setTodos(toggleTodo(todos, index))}
+						>
+							Completa
+						</button>
+					</li>
+				))}
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
